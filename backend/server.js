07e53448884b2577d98d70be38b34cc0542304b5
Carter McKie <mckiece@gmail.com>
2022-02-25 09:35:35 -0500

    add basic backend functionality, routes, middleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("server...");
+const express = require("express");
+const connectDB = require("./config/db");
+const colors = requier("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+	res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
