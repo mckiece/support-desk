0e1c11bb0a96320452662f231f8f9819399c8940
Carter McKie <mckiece@gmail.com>
2022-02-28 12:35:51 -0500

    Add update and delete for tickets on backend

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,6 +3,8 @@ const {
 	getTickets,
 	createTicket,
 	getTicket,
+	deleteTicket,
+	updateTicket,
 } = require("../controllers/ticketController");
 const router = express.Router();
 
@@ -10,6 +12,10 @@ const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
 
-router.route("/:id").get(protect, getTicket);
+router
+	.route("/:id")
+	.get(protect, getTicket)
+	.delete(protect, deleteTicket)
+	.put(protect, updateTicket);
 
 module.exports = router;
