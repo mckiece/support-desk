92153edbba3bdd61bb0881dd436821d6e6a3fff6
Carter McKie <mckiece@gmail.com>
2022-02-28 12:27:14 -0500

    Add get single ticket

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+	getTickets,
+	createTicket,
+	getTicket,
+} = require("../controllers/ticketController");
 const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router.route("/:id").get(protect, getTicket);
 
 module.exports = router;
